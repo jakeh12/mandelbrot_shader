456a05ee14f6e0362a89f9ebe259120e13c3bb91
Jakub Hladik <jhladik@iastate.edu>
2018-08-02 20:31:02 -0500

    Initial Commit

---
+++ mandelbrot_shader/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mandelbrot_shader
+//
+//  Created by Jakub Hladik on 8/2/18.
+//  Copyright © 2018 Jakub Hladik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
